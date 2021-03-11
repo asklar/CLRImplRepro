df074848795aa72ae94dc4392f849161dd087ec4
Alexander Sklar <asklar@microsoft.com>
2021-03-10 23:56:17 -0800

    Repro

---
+++ CSharpComponent/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpComponent
+{
+    public interface Interface1
+    {
+        void f();
+    }
+    public interface Interface2
+    {
+        void g();
+    }
+    public sealed class Class1 : Interface1, Interface2
+    {
+        public void f() { }
+        public void g() { }
+    }
+}
